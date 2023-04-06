f3c558157e1d9e193b3e0847b43887809d2999c7
itheima <itheima@itcast.cn>
2023-04-06 18:17:05 +0800

    完成了登录和注册功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,67 +1,65 @@
 $(function(){
-    // alert('hello')
-    // 点击”去注册账号的连接“
-    $('#link_reg').on('click',function() {
+    $('#link_reg').on('click',function(){
         $('.login-box').hide()
         $('.reg-box').show()
     })
-    
-    // 点击”去登录“的连接
-    $('#link_login').on('click',function() {
+    // 点击”去登陆“的链接
+    $('#link_login').on('click',function(){
         $('.login-box').show()
         $('.reg-box').hide()
     })
 
-
-
-    //从 layui 中获取 form 对象
+    // 从layui中获取from对象
     var form = layui.form
-    // var layer = layui.msg
-    // 通过form.verify()函数自定义校验规则
+    var layer = layui.layer
+  
+    // 通过 form.verify()函数自定义校验规则
     form.verify({
-        pwd: [/^[\S]{6,12}$/,'密码必须6到12位,且不能出现空格'],
-        // 校验两次密码是否一致的规则
+        pwd:[/^[\S]{6,12}$/,'密码必须6到12位。且不能出现空格']
+        ,
+        // 校验两次密码是否一致
         repwd: function(value){
             // 通过形参拿到的是确认密码框中的内容
             // 还需要拿到密码框中的内容
             // 然后进行一次等于的判断
-            // 如果判断失败,则return一个提示消息
-           var pwd=$('.reg-box [name=password]').val()
-           if(pwd!==value)
-           return '两次密码不一致!'
+            // 如果判断失败，则return一个提示消息即可
+          var pwd=$('.reg-box [name=password]').val()
+          if(pwd !== value){
+            return '两次密码不一致！'
+          }
         }
-    
     })
-    
-    // 监听注册表单的提交事件
-    $('#form_reg').on('submit',function(e){
-        e.preventDefault() 
-        $.post('/api/reguser',{username: $('#form_reg [name=username]').val(),password: $('#form_reg [name=password]').val()},function(res){
-                if(res.status !==0){
-                    return console.log(res.message);
-                }
-                console.log('注册成功!');
-                // 模拟点击行为
-                $('#link_login').click()
 
+    //监听注册表单的提交事件
+    $('#form_reg').on('submit',function (event){
+        // 1、先阻止默认的提交行为
+        event.preventDefault()
+        // 2、发起Ajax的POST请求
+        var data = {username:$('#form_reg [name=username]').val(),password:$('#form_reg [name=password]').val()}
+        $.post('http://www.liulongbin.top:3007/api/reguser',data,function(res){
+            if(res.status!==0){
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功,去登陆！')
+            // 模拟人的点击行为
+            $('#link_login').click()
         })
     })
 
     // 监听登录表单的提交事件
-    $('#form_login').submit(function(e){
-        // 组织默认提交行为
-        e.preventDefault()
+    $('#form_login').submit(function(event){
+        event.preventDefault()
         $.ajax({
             url:'/api/login',
             method:'POST',
             // 快速获取表单中的数据
-            data: $(this).serialize(),
+            data:$(this).serialize(),
             success: function(res){
-                if(res.status !== 0 ){
-                    return console.log('登录失败!');
+                if(res.status !== 0){
+                return layer.msg('登陆失败')
                 }
-                console.log('登录成功! ');
-                // 将登录成功得到的token字符串,保存在localStorage本地存储中
+                layer.msg('登陆成功！')
+                // 将登录成功得到的token字符串，保存到localStorage中
                 localStorage.setItem('token',res.token)
                 // console.log(res.token);
                 // 跳转到后台主页
